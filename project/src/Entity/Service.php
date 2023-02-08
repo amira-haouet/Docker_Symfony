6df7908d09fdc55191d5062c5332a3bbb48feb33
amira-haouet <haouetamira@gmail.com>
2023-02-08 15:05:41 +0100

    add entity & fields

---
+++ project/src/Entity/Service.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ServiceRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ServiceRepository::class)
+ */
+class Service
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $label;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $sub_label;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLabel(): ?string
+    {
+        return $this->label;
+    }
+
+    public function setLabel(string $label): self
+    {
+        $this->label = $label;
+
+        return $this;
+    }
+
+    public function getSubLabel(): ?string
+    {
+        return $this->sub_label;
+    }
+
+    public function setSubLabel(string $sub_label): self
+    {
+        $this->sub_label = $sub_label;
+
+        return $this;
+    }
+}
